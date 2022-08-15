390e5813bebb667158f935ae6d7502435d4cfe5f
LMNatale1967 <LuisMiguelNatale@gmail.comm>
2022-08-15 08:13:08 +0200

    New Commit with @Input & $Event Parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+
+  @Input() firstName!: string;
+  @Input() lastName!: string;
+  @Input() fullName!: string;
+
   title = 'WW_TypeScript_01';
+
+
+  displayMyFirstName(pFirstname:string, pEvent:Event): void {
+    alert ("The firstname is : " + pFirstname);
+    console.log(pEvent);
+  }
+
+  displayMyLastName(pLastName:string, pEvent:Event): void {
+    alert ("The lastname is : " + pLastName);
+    console.log(pEvent);
+  }
+
+  displayMyFullName(pFullName:string, pEvent:Event): void {
+    alert ("The fullname is : " + pFullName);
+    console.log(pEvent);
+  }
 }
